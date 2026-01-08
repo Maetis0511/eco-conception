1af0e62b28125c871470612038e698a5f4bd411c
Groupe Vincent <lionel.martin@groupevincent.fr>
2026-01-08 14:34:27 +0100

    Responsive administration + index

--- controleur/traitementFormConnexion.php
+++ controleur/traitementFormConnexion.php
@@ -4,14 +4,14 @@ require("../metier/DB_connector.php");
 require("../metier/User.php");
 require("../Dao/UserDao.php");
 
-if (isset($_GET['idUtil']) || isset($_GET['mdpUtil'])) {
+if (isset($_POST['idUtil']) || isset($_POST['mdpUtil'])) {
 
     $cnx = new DB_Connector();
     $jeton = $cnx->openConnexion();
 
     $userManager = new UserDao($jeton);
-    $userId = $_GET['idUtil'];
-    $mdp = $_GET['mdpUtil'];
+    $userId = $_POST['idUtil'];
+    $mdp = $_POST['mdpUtil'];
 	
     $jetonExistance = $userManager->userExist($userId, MD5($mdp));
     if ($jetonExistance) {
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,9 @@
 <nav class="navbar">
-    <img src="./images/scierie.gif" alt='img' style="width:70px; margin:5px;">
+    <img src="./images/scierie.gif" style="width:70px; margin:5px;">
 
     <li class="toggle">
-        <ul class="toggle-item"><i class="fa fa-bars menu" aria-hidden="true"></i></ul>
+        <ul class ="toggle-item"><i class="fa fa-bars menu" aria-hidden="true"> </i></ul>
     </li>
-
     <ul class="nav-links">
         <li class="nav-item"><a href="index.php">ACCUEIL</a></li>
         <li class="nav-item"><a href="produits.php">LES PRODUITS</a></li>
@@ -15,25 +14,15 @@
             echo "<li class='nav-item'><a href='administration.php'>ADMINISTRATION</a></li>";
         }
         ?>
-
-        <li class="nav-item mobile-auth">
-            <?php
-            if(isset($_SESSION['id'])) {
-                echo "<a href='deconnexion.php' class='link-logout'>DECONNEXION</a>";
-            } else {
-                echo "<a href='connexion.php' class='link-login'>CONNEXION</a>";
-            }
-            ?>
-        </li>
     </ul>
 
-    <ul class="connexion desktop-auth">
+    <div class="connexion">
         <?php
         if(isset($_SESSION['id'])) {
             echo "<li class='nav-item'><a href='deconnexion.php'>DECONNEXION</a></li>";
-        } else {
+        }else{
             echo "<li class='nav-item'><a href='connexion.php'>CONNEXION</a></li>";
         }
         ?>
-    </ul>
+    </div>
 </nav>
