c7672b901b19760f516da692b5886627aa11161d
Julien <juliendurand77140@gmail.com>
2026-01-08 15:20:02 +0100

    fix css

--- connexion.php
+++ connexion.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="fr">
 <head>
     <title>Connexion / Inscription</title>
-    <meta charset="UTF-8">
+    <meta name="description" content="Contact page">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="style.css">
@@ -85,9 +85,7 @@ session_start();
         }
 
         /* Si tu préfères le texte blanc par défaut sur le bouton vert : */
-        .btn-scierie {
-            color: white;
-        }
+        .btn-scierie { color: white; }
 
         /* Au survol : Fond blanc, texte vert (comme ta navbar) */
         .btn-scierie:hover {
@@ -110,14 +108,12 @@ session_start();
                 <div class="card-header p-0">
                     <ul class="nav nav-tabs nav-fill" id="authTab" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link active" id="login-tab" data-toggle="tab" href="#loginPanel" role="tab"
-                               aria-controls="loginPanel" aria-selected="true">
+                            <a class="nav-link active" id="login-tab" data-toggle="tab" href="#loginPanel" role="tab" aria-controls="loginPanel" aria-selected="true">
                                 <i class="fas fa-sign-in-alt"></i> Connexion
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="register-tab" data-toggle="tab" href="#registerPanel" role="tab"
-                               aria-controls="registerPanel" aria-selected="false">
+                            <a class="nav-link" id="register-tab" data-toggle="tab" href="#registerPanel" role="tab" aria-controls="registerPanel" aria-selected="false">
                                 <i class="fas fa-user-plus"></i> Inscription
                             </a>
                         </li>
@@ -158,8 +154,7 @@ session_start();
 
                     <div class="tab-content" id="authTabContent">
 
-                        <div class="tab-pane fade show active" id="loginPanel" role="tabpanel"
-                             aria-labelledby="login-tab">
+                        <div class="tab-pane fade show active" id="loginPanel" role="tabpanel" aria-labelledby="login-tab">
                             <h4 class="text-center mb-4 mt-2" style="color:#54b454;">Connexion</h4>
 
                             <form action="controleur/traitementFormConnexion.php" method="POST">
@@ -169,8 +164,7 @@ session_start();
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="fas fa-user"></i></span>
                                         </div>
-                                        <input type="text" class="form-control" name="idUtil" id="idUtil"
-                                               placeholder="Votre nom d'utilisateur" required>
+                                        <input type="text" class="form-control" name="idUtil" id="idUtil" placeholder="Votre nom d'utilisateur" required>
                                     </div>
                                 </div>
 
@@ -180,8 +174,7 @@ session_start();
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="fas fa-lock"></i></span>
                                         </div>
-                                        <input type="password" class="form-control" name="mdpUtil" id="mdpUtil"
-                                               placeholder="Votre mot de passe" required>
+                                        <input type="password" class="form-control" name="mdpUtil" id="mdpUtil" placeholder="Votre mot de passe" required>
                                     </div>
                                 </div>
 
@@ -201,8 +194,7 @@ session_start();
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="fas fa-user-plus"></i></span>
                                         </div>
-                                        <input type="text" class="form-control" name="idUtilCreation"
-                                               id="idUtilCreation" placeholder="Ex: JeanDupont" required>
+                                        <input type="text" class="form-control" name="idUtilCreation" id="idUtilCreation" placeholder="Ex: JeanDupont" required>
                                     </div>
                                 </div>
 
@@ -232,8 +224,7 @@ session_start();
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="fas fa-check-circle"></i></span>
                                         </div>
-                                        <input type="password" class="form-control" name="pwdBis" id="pwdBis"
-                                               placeholder="Répétez le mot de passe" required>
+                                        <input type="password" class="form-control" name="pwdBis" id="pwdBis" placeholder="Répétez le mot de passe" required>
                                     </div>
                                 </div>
 
@@ -243,10 +234,7 @@ session_start();
                             </form>
                         </div>
 
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </div> </div> </div> </div>
     </div>
 </div>
 
@@ -256,8 +244,8 @@ session_start();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
-    $(document).ready(function () {
-        $('.menu').click(function () {
+    $(document).ready(function(){
+        $('.menu').click(function(){
             $('ul').toggleClass('active');
         });
     });
